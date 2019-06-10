5000331a2fade8934cb0084bd272a0b3aedc7965
Sam Ruby <rubys@intertwingly.net>
2019-06-10 23:48:33 +0800

    Depot Scaffold

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :products
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get "/product", to: "products#index"
+  #get "/products", to: "products#show"
+  #get "/products/new", to: "products#new"
+end
