9e17b9e20bdcda57b9f59fd856fb9ce6406e81dd
kdanuser <kdanuser@kdanuserde-MacBook-Air.local>
2019-07-17 18:06:10 +0800

    20170717

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'store/index'
   #root 'store#index', as: 'store_index'
   resources :products
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get "/product", to: "products#index"
   #get "/products", to: "products#show"
