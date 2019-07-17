f2f60da033fd18ca0df1c6034d1c43055a7cd9bc
kdanuser <kdanuser@kdanuserde-MacBook-Air.local>
2019-07-17 14:12:18 +0800

    20190717

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -33,7 +33,9 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to store_index_url }
+        #format.html { redirect_to @line_item.cart }
+        #format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         #format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
