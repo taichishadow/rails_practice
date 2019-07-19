71bc043ad84de136939beaf2db8179578e4dfc3e
kdanuser <kdanuser@kdanuserde-MacBook-Air.local>
2019-07-19 10:32:17 +0800

    201890719

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,12 +29,13 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    @order.add_line_items_from_cart(@cart)
 
     respond_to do |format|
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        format.html { redirect_to @order, notice: 'Thank you for your order.' }
+        format.html { redirect_to store_index_url, notice: 'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
